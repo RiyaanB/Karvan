1dad1286e08bd59771d23398c1c90c9947ea7622
Riyaan Bakhda <ronny@Riyaans-MacBook-Air.local>
2019-03-10 21:58:30 +0530

    New blank activities

---
+++ app/src/main/java/com/example/karvan/DeactivateTwoActivity.java
@@ -0,0 +1,13 @@
+package com.example.karvan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeactivateTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deactivate_two);
+    }
+}
---
+++ app/src/main/java/com/example/karvan/PrimarySearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.karvan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimarySearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary_search);
+    }
+}
